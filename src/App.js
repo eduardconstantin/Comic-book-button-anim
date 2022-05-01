053b0985fbb40763903ce038fc0a33c058521ce1
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2022-05-01 11:49:46 +0300

    added project files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import style from './style/App.module.css';
+import Button from './components/Button';
+
+export default function App() {
+  return (
+    <div className={style.App}>
+      <Button/>
+    </div>
+  );
+}
