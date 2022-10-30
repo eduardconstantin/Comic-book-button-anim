b1d9fe06794c82d2ff48d29a8e32276e727500b9
Tushar Singh <tusharvickey1999@gmail.com>
2022-10-30 19:37:40 +0530

    Add ComicButton styled GitHub stargazer counter (#38)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ComicButton from "./components/ComicButton/ComicButton";
 import Counter from "./components/Counter/Counter";
+import GithubStargazers from "./components/GithubStargazers/GithubStargazers"
 
 export default function App() {
   const [count, setCount] = useState<number>(0);
@@ -9,13 +10,18 @@ export default function App() {
   };
   return (
     <div className="App">
-      <div className="buttonContainer">
-        <ComicButton
-          buttonName="BUTTON"
-          hoverBtnName="HOVER"
-          handleButtonClick={handleButtonClick}
-        />
-        <Counter count={count} />
+      <div className="appContainer">
+        <div className="ghStargazersContainer">
+          <GithubStargazers />
+        </div>
+        <div className="buttonContainer">
+          <ComicButton
+            buttonName="BUTTON"
+            hoverBtnName="HOVER"
+            handleButtonClick={handleButtonClick}
+          />
+          <Counter count={count} />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/GithubStargazers/GithubStargazers.tsx
@@ -0,0 +1,90 @@
+import {useEffect, useLayoutEffect, useState, useRef} from "react"
+import ComicButton from "../ComicButton/ComicButton"
+import style from "./GithubStargazers.module.css"
+
+function GithubStargazers() {
+  const [stars, setStars] = useState<number | null>(null);
+  const ghStargazersRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (stars!=null) return
+    fetchStars().then((ghStars) => setStars(ghStars))
+  }, [stars])
+
+  // layoutEffect will avoid cases when button might be visible without
+  // the icon for a brief duration due to how useEffect works.
+  useLayoutEffect(() => {
+    if (!ghStargazersRef.current) return
+    addGithubIcon(ghStargazersRef.current, style.ghStarBtn, style.ghIcon)
+  }, [stars])
+
+  return  (
+    <div ref={ghStargazersRef} className="ghStargazers">
+      <a href="https://github.com/eduardconstantin/Comic-book-button-anim">
+        {stars != null ? 
+          <ComicButton
+            buttonName={"stars: " + stars}
+            hoverBtnName="source"
+            btnClassName={style.ghStarBtn}
+            handleButtonClick={()=> {}}
+          />
+          : null
+        }
+      </a>
+    </div>
+  )
+}
+
+async function fetchStars(): Promise<number | null> {
+  const url = "https://api.github.com/repos/eduardconstantin/Comic-book-button-anim"
+  let resp 
+  try {
+    resp = await fetch(url, {
+      headers: {
+        "Accept": 'application/vnd.github+json',
+      },
+    })
+  } catch(e) {
+    console.warn("could not fetch star count")
+    return null
+  }
+
+  if (!resp.ok) {
+    console.warn("could not fetch star count")
+    return null
+  }
+  let data = await resp.json() as GithubResp
+  return data["stargazers_count"] ?? null
+}
+
+/**
+ * 
+ * @param compEl GithubStargazers component 
+ * @param btnClassName ComicButton className to find its node
+ * @param iconClassName icon's className for additional styling
+ */
+
+function addGithubIcon(compEl: HTMLElement, btnClassName: string, iconClassName?: string) {
+  // dont add icon if already added incase Effect runs again
+  if(compEl.dataset.iconAdded) return
+
+  const btnEl = compEl.querySelector("." + btnClassName)
+  if (!btnEl) return
+
+  const iconImg = document.createElement("img")
+  iconImg.src = "/github.png";
+  iconImg.alt = "github icon"
+  if(iconClassName) iconImg.classList.add(iconClassName);
+
+  btnEl.insertBefore(iconImg, btnEl.firstChild)
+  // hints icon is added
+  compEl.dataset.iconAdded = "1"
+}
+
+
+interface GithubResp {
+  ["stargazers_count"]: number,
+  [key: string]: any
+}
+
+export default GithubStargazers
